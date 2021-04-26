ca6d38f79ae7ab1424559c1c32198785c6b48751
aksem62 <aksem62@gmail.com>
2021-04-26 18:21:56 +0300

    make axios request on submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState({ articles: [] });
+  const [quest, setQuest] = useState("COVID");
+  const [submitted, setSubmitted] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchNews = async () => {
+      setIsLoading(true);
+      const result = await axios(
+        `https://newsapi.org/v2/top-headlines?q=${quest}&apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      setTimeout(() => setIsLoading(false), 5000);
+      // setIsLoading(false);
+      setNews(result.data);
+    };
+    submitted && fetchNews();
+  }, [quest, submitted]);
+
+  const search = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="heading">
+          <h2>Grid styled news api app</h2>
+        </div>
+        <form onSubmit={search}>
+          <label htmlFor="question">Enter search phrase </label>
+          <input
+            id="question"
+            value={quest}
+            type="text"
+            onChange={(e) => {
+              setSubmitted(false);
+              setQuest(e.target.value);
+            }}
+          />
+          <button type="submit">Search</button>
+        </form>
+        {!isLoading ? (
+          <div className="news-block">
+            {news.articles.map((article) => (
+              <div key={uuidv4()} className="news-card">
+                <div className="news-card-title">{article.title}</div>
+                <div className="news-card-description">
+                  {article.description}
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="loading">Loading...</div>
+        )}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
